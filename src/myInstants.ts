d34d197b628d628c0fbf449659fb7d235e40ca64
Vojtěch Baránek <vojtabaranek@gmail.com>
2025-10-29 09:58:20 +0100

    add more sounds

--- src/myInstants.ts
+++ src/myInstants.ts
@@ -15,6 +15,43 @@ export const myInstantsDefinition={
       "name": "Helicopter helicopter",
       "filename": "helicopter-meme.mp3"
     },
+    {
+      "name": "Czechcloud dopici",
+      "filename": "czechcloud.mp3"
+    },
+ {
+      "name": "Vine boom",
+      "filename": "vine-boom.mp3"
+    },
+    {
+      "name": "Anime wow",
+      "filename": "anime-wow-sound-effect.mp3"
+    }, {
+      "name": "Brainrot ringtone ",
+      "filename": "italian-brainrot-ringtone.mp3"
+    },{
+      "name": "Meme final",
+      "filename": "meme-de-creditos-finales.mp3"
+    },
+{
+      "name": "Buzzer",
+      "filename": "wrong-answer-sound-effect.mp3"
+    },{
+      "name": "Mouse click",
+      "filename": "mouse-click-sound.mp3"
+    },{
+      "name": "Sad violin",
+      "filename": "tf_nemesis.mp3"
+    },{
+      "name": "emotional damage",
+      "filename": "emotional-damage-meme.mp3"
+    },{
+      "name": "Ruby hovno",
+      "filename": "ruby-hovno-mp3cut.mp3"
+    },{
+      "name": "You got jebaited",
+      "filename": "jebaitedsoundboard.mp3"
+    },
 
   ],
 
--- src/theChatWheel.ts
+++ src/theChatWheel.ts
@@ -8,12 +8,21 @@ export const theChatWheelDefinition={
     "url": "https://dota2.fandom.com/wiki/Chat_Wheel"
   },
 
-  "rootUrl": "https://static.wikia.nocookie.net/dota2_gamepedia/images/c/c4/",
+  "rootUrl": "https://static.wikia.nocookie.net/",
 
   "tiles": [
     {
       "name": "Sad trombone",
-      "filename": "Misc_soundboard_sad_bone.mp3"
+      "filename": "dota2_gamepedia/images/c/c4/Misc_soundboard_sad_bone.mp3"
+    }, {
+      "name": "CryBaby",
+      "filename": "dota2_gamepedia/images/5/51/Misc_soundboard_crybaby.mp3"
+    },{
+      "name": "Waow",
+      "filename": "dota2_gamepedia/images/6/61/Misc_soundboard_wow.mp3"
+    },{
+      "name": "Badum tss",
+      "filename": "dota2_gamepedia/images/f/fc/Misc_soundboard_rimshot.mp3"
     },
 
   ],
