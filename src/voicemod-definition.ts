a41ce7e8ebac1a5d806b9cc05e984504859c33ae
Jiri Vala <jiri.vala@finshape.com>
2025-10-30 14:41:52 +0100

    fix: urls

--- src/voicemod-definition.ts
+++ src/voicemod-definition.ts
@@ -6,7 +6,6 @@ export const voicemodDefinitions = {
     lang: 'cs',
     version: '1.0.0',
     color: '#0e3f88ff',
-    // 3c687a4d-9137-4ea9-a247-2871b4a82c1c-1754854085649.mp3
     rootUrl: 'https://us-tuna-sounds-files.voicemod.net/',
     author: {
         name: 'Sám Život',
@@ -41,15 +40,15 @@ export const voicemodDefinitions = {
         },
         {
             name: 'Yippee!',
-            filename: '41cf4fdf-deb0-455f-a2f5-34aab48df5b6.mp3',
+            filename: '41cf4fdf-deb0-455f-a2f5-34aab48df5b6-1677820708227.mp3',
         },
         {
             name: 'Spongebob music',
-            filename: 'db14a9ce-811a-42e2-8e56-a64ac797efec.mp3',
+            filename: 'aae18a58-8405-46d3-8e77-abe1da3a977d-1759975584546.mp3',
         },
         {
             name: 'Noice!',
-            filename: '0bcf33b8-a861-4f67-b968-12cb8edde903.mp3',
+            filename: '0bcf33b8-a861-4f67-b968-12cb8edde903-1661705491815.mp3',
         },
         {
             name: 'Nani?!',
@@ -61,103 +60,99 @@ export const voicemodDefinitions = {
         },
         {
             name: 'yeet!',
-            filename: 'dae93ef7-3f6c-40dc-971a-a82ead461119.mp3',
-        },
-        {
-            name: 'spongebob grass skirt chase music',
-            filename: 'aae18a58-8405-46d3-8e77-abe1da3a977d.mp3',
+            filename: 'dae93ef7-3f6c-40dc-971a-a82ead461119-1664580430247.mp3',
         },
         {
             name: 'What did he sayyy',
-            filename: 'fa02499c-714d-4e63-a588-ebecbfd6b5c0.mp3',
+            filename: 'fa02499c-714d-4e63-a588-ebecbfd6b5c0-1680811716822.mp3',
         },
         {
             name: 'I can smell you',
-            filename: '9e707120-6960-4abf-ab2c-2b2377adc06c.mp3',
+            filename: '9e707120-6960-4abf-ab2c-2b2377adc06c-1661985926990.mp3',
         },
         {
             name: 'oh my god bro ah hell nah man',
-            filename: '535dbf78-19b6-4707-a428-675a15eef25e.mp3',
+            filename: '535dbf78-19b6-4707-a428-675a15eef25e-1709764505069.mp3',
         },
         {
             name: 'Max Verstappen theme',
-            filename: '98e88d8e-dde3-45c5-8f55-cd6936f92079.mp3',
+            filename: '98e88d8e-dde3-45c5-8f55-cd6936f92079-1713803180525.mp3',
         },
         {
             name: 'John Cena theme',
-            filename: 'df13df51-1122-400d-9bab-3964bc1fb527.mp3',
+            filename: 'df13df51-1122-400d-9bab-3964bc1fb527-1672226885572.mp3',
         },
         {
             name: 'Nigga what',
-            filename: '95633157-13c1-47db-a233-d56bbd709659.mp3',
+            filename: '95633157-13c1-47db-a233-d56bbd709659-1691897192393.mp3',
         },
         {
             name: 'Disgusting!',
-            filename: '27a0c67c-b134-40ef-8cd4-94f9ded53248.mp3',
+            filename: '27a0c67c-b134-40ef-8cd4-94f9ded53248-1761337152243.mp3',
         },
         {
             name: 'Tak ses uplnej curak?!',
-            filename: '03178326-1e17-48a4-b046-049981915c3c.mp3',
+            filename: '03178326-1e17-48a4-b046-049981915c3c-1687710692159.mp3',
         },
         {
             name: 'Meme de creditos',
-            filename: '73ec7bbd-65f3-4f0e-892c-219ab5002ade.mp3',
+            filename: 'dbbc5e03-4324-4a07-ac4d-91e5c6561a45-1675898290795.mp3',
         },
         {
             name: 'Nastup postizeneho',
-            filename: 'badab355-c816-4daf-a95f-ef6118ef9784.mp3',
+            filename: 'badab355-c816-4daf-a95f-ef6118ef9784-1753453510093.mp3',
         },
         {
             name: 'Drz picu nikoho to nezajima',
-            filename: '99a4854f-57de-41a1-8619-84e411485fc3.mp3',
+            filename: '99a4854f-57de-41a1-8619-84e411485fc3-1752651441628.mp3',
         },
         {
             name: 'Ty ses pica blba',
-            filename: '9708eebe-0d8b-47f2-8bbe-c8ea8313038d.mp3',
+            filename: '08e1c54d-96c7-470c-a281-2d991230de77-1754521942036.mp3',
         },
         {
             name: 'Hlavne no stress',
-            filename: '5d7e7ea6-83c3-4530-8dee-d9a80772cee4.mp3',
+            filename: '5d7e7ea6-83c3-4530-8dee-d9a80772cee4-1713973687522.mp3',
         },
         {
             name: 'Zdravim zdravim',
-            filename: '02168bc8-3600-423d-8f29-1117288e4447.mp3',
+            filename: '02168bc8-3600-423d-8f29-1117288e4447-1712337664243.mp3',
         },
         {
             name: 'Za to muzes ty!',
-            filename: '80876924-5f8b-494f-a796-e69a2fd6ee9b.mp3',
+            filename: '80876924-5f8b-494f-a796-e69a2fd6ee9b-1709483705150.mp3',
         },
         {
             name: 'No nazdar!',
-            filename: '92e4a162-11bb-41aa-a9c0-06714dccc2fa.mp3',
+            filename: '92e4a162-11bb-41aa-a9c0-06714dccc2fa-1705073368389.mp3',
         },
         {
             name: 'Zdravim - Stiffler',
-            filename: '011edc42-86a6-480a-a685-dec56325b27b.mp3',
+            filename: '011edc42-86a6-480a-a685-dec56325b27b-1700248475868.mp3',
         },
         {
             name: 'Zdar bila hubo',
-            filename: 'b8a6d44c-400b-40ff-bc09-4b679fe25111.mp3',
+            filename: '531f5e73-f9f8-4b15-9717-ee8a9980b48d-1699725723925.mp3',
         },
         {
             name: 'Kundolap',
-            filename: '81d6cb9e-bf3b-4fe4-ab5c-236c91875ad6.mp3',
+            filename: '81d6cb9e-bf3b-4fe4-ab5c-236c91875ad6-1699725940374.mp3',
         },
         {
             name: 'Babis - Jede prezident',
-            filename: '75350b7f-313d-4f43-b42a-78d026fe2472.mp3',
+            filename: '75350b7f-313d-4f43-b42a-78d026fe2472-1673903296883.mp3',
         },
         {
             name: 'Coze???',
-            filename: '631a741b-c443-47be-bd88-bc2c7d44bd0a.mp3',
+            filename: '631a741b-c443-47be-bd88-bc2c7d44bd0a-1669480004111.mp3',
         },
         {
             name: 'Kurva seru na to!',
-            filename: '86a15f20-5ce8-4b6f-9558-342e42ce07bd.mp3',
+            filename: '86a15f20-5ce8-4b6f-9558-342e42ce07bd-1660491400741.mp3',
         },
         {
             name: 'Oh no! Cringe!',
-            filename: 'fd359da4-a08c-4ed4-878c-3856ed277083.mp3',
+            filename: '11991ab9-9e05-41c9-b572-66950b90dbc9-1644098977332.mp31',
         },
     ],
     references: [
