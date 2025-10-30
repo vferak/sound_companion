7403bffe868afd120ca2fddca2307a20ae57b5f4
Jiri Vala <jiri.vala@finshape.com>
2025-10-30 13:49:20 +0100

    feat: add sounds from voicemod

---
+++ src/voicemod-definition.ts
@@ -0,0 +1,169 @@
+export const voicemodDefinitions = {
+    name: 'Tuna by Voicemod',
+    shortName: 'Voicemod',
+    description: 'Hehe',
+    story: ['Hehe.'],
+    lang: 'cs',
+    version: '1.0.0',
+    color: '#0e3f88ff',
+    // 3c687a4d-9137-4ea9-a247-2871b4a82c1c-1754854085649.mp3
+    rootUrl: 'https://us-tuna-sounds-files.voicemod.net/',
+    author: {
+        name: 'Sám Život',
+        url: 'https://aquasoup.com',
+    },
+    icons: [
+        {
+            src: '/icons/og-image.jpg',
+            sizes: '1024x1024',
+            type: 'image/jpeg',
+        },
+        {
+            src: '/icons/icon-48.png',
+            sizes: '48x48',
+            type: 'image/png',
+        },
+        {
+            src: '/icons/icon-144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'maskable any',
+        },
+    ],
+    tiles: [
+        {
+            name: 'Accepting fate',
+            filename: '3c687a4d-9137-4ea9-a247-2871b4a82c1c-1754854085649.mp3',
+        },
+        {
+            name: 'No no no no, wait wait wait wait',
+            filename: '4ef80b20-c3c8-4f28-9124-b077fa5a93df-1704783502307.mp3',
+        },
+        {
+            name: 'Yippee!',
+            filename: '41cf4fdf-deb0-455f-a2f5-34aab48df5b6.mp3',
+        },
+        {
+            name: 'Spongebob music',
+            filename: 'db14a9ce-811a-42e2-8e56-a64ac797efec.mp3',
+        },
+        {
+            name: 'Noice!',
+            filename: '0bcf33b8-a861-4f67-b968-12cb8edde903.mp3',
+        },
+        {
+            name: 'Nani?!',
+            filename: 'd2a88c89-2b49-4708-bbdc-5c3ea17902a2.mp3',
+        },
+        {
+            name: 'Hello there!',
+            filename: '332cb7e1-a52f-4fa9-8f71-208c67be2b3b.mp3',
+        },
+        {
+            name: 'yeet!',
+            filename: 'dae93ef7-3f6c-40dc-971a-a82ead461119.mp3',
+        },
+        {
+            name: 'spongebob grass skirt chase music',
+            filename: 'aae18a58-8405-46d3-8e77-abe1da3a977d.mp3',
+        },
+        {
+            name: 'What did he sayyy',
+            filename: 'fa02499c-714d-4e63-a588-ebecbfd6b5c0.mp3',
+        },
+        {
+            name: 'I can smell you',
+            filename: '9e707120-6960-4abf-ab2c-2b2377adc06c.mp3',
+        },
+        {
+            name: 'oh my god bro ah hell nah man',
+            filename: '535dbf78-19b6-4707-a428-675a15eef25e.mp3',
+        },
+        {
+            name: 'Max Verstappen theme',
+            filename: '98e88d8e-dde3-45c5-8f55-cd6936f92079.mp3',
+        },
+        {
+            name: 'John Cena theme',
+            filename: 'df13df51-1122-400d-9bab-3964bc1fb527.mp3',
+        },
+        {
+            name: 'Nigga what',
+            filename: '95633157-13c1-47db-a233-d56bbd709659.mp3',
+        },
+        {
+            name: 'Disgusting!',
+            filename: '27a0c67c-b134-40ef-8cd4-94f9ded53248.mp3',
+        },
+        {
+            name: 'Tak ses uplnej curak?!',
+            filename: '03178326-1e17-48a4-b046-049981915c3c.mp3',
+        },
+        {
+            name: 'Meme de creditos',
+            filename: '73ec7bbd-65f3-4f0e-892c-219ab5002ade.mp3',
+        },
+        {
+            name: 'Nastup postizeneho',
+            filename: 'badab355-c816-4daf-a95f-ef6118ef9784.mp3',
+        },
+        {
+            name: 'Drz picu nikoho to nezajima',
+            filename: '99a4854f-57de-41a1-8619-84e411485fc3.mp3',
+        },
+        {
+            name: 'Ty ses pica blba',
+            filename: '9708eebe-0d8b-47f2-8bbe-c8ea8313038d.mp3',
+        },
+        {
+            name: 'Hlavne no stress',
+            filename: '5d7e7ea6-83c3-4530-8dee-d9a80772cee4.mp3',
+        },
+        {
+            name: 'Zdravim zdravim',
+            filename: '02168bc8-3600-423d-8f29-1117288e4447.mp3',
+        },
+        {
+            name: 'Za to muzes ty!',
+            filename: '80876924-5f8b-494f-a796-e69a2fd6ee9b.mp3',
+        },
+        {
+            name: 'No nazdar!',
+            filename: '92e4a162-11bb-41aa-a9c0-06714dccc2fa.mp3',
+        },
+        {
+            name: 'Zdravim - Stiffler',
+            filename: '011edc42-86a6-480a-a685-dec56325b27b.mp3',
+        },
+        {
+            name: 'Zdar bila hubo',
+            filename: 'b8a6d44c-400b-40ff-bc09-4b679fe25111.mp3',
+        },
+        {
+            name: 'Kundolap',
+            filename: '81d6cb9e-bf3b-4fe4-ab5c-236c91875ad6.mp3',
+        },
+        {
+            name: 'Babis - Jede prezident',
+            filename: '75350b7f-313d-4f43-b42a-78d026fe2472.mp3',
+        },
+        {
+            name: 'Coze???',
+            filename: '631a741b-c443-47be-bd88-bc2c7d44bd0a.mp3',
+        },
+        {
+            name: 'Kurva seru na to!',
+            filename: '86a15f20-5ce8-4b6f-9558-342e42ce07bd.mp3',
+        },
+        {
+            name: 'Oh no! Cringe!',
+            filename: 'fd359da4-a08c-4ed4-878c-3856ed277083.mp3',
+        },
+    ],
+    references: [
+        {
+            name: 'Reference',
+            url: 'https://tuna.voicemod.net/search/sounds/meme/?searchOrigin=search-pill&searchPosition=0',
+        },
+    ],
+};
