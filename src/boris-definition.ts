33e7fe888401f9838319db881f628de723522726
Martin Borák <martin.borak24@gmail.com>
2025-11-03 08:38:04 +0100

    feat: boris definition

---
+++ src/boris-definition.ts
@@ -0,0 +1,44 @@
+export const voicemodDefinitions = {
+  name: "Boris-webserver",
+  shortName: "Boris",
+  description: "-",
+  story: ["-"],
+  lang: "cs",
+  version: "1.0.0",
+  color: "#0e3f88ff",
+  rootUrl: "https://wailord.org/assets/audio",
+  author: {
+    name: "Boris",
+    url: "https://wailord.org",
+  },
+  icons: [
+    {
+      src: "/icons/og-image.jpg",
+      sizes: "1024x1024",
+      type: "image/jpeg",
+    },
+    {
+      src: "/icons/icon-48.png",
+      sizes: "48x48",
+      type: "image/png",
+    },
+    {
+      src: "/icons/icon-144.png",
+      sizes: "144x144",
+      type: "image/png",
+      purpose: "maskable any",
+    },
+  ],
+  tiles: [
+    {
+      name: "A abys věděl, že to myslím vážně",
+      filename: "pobodam_fotra.mp3",
+    },
+  ],
+  references: [
+    {
+      name: "Reference",
+      url: "https://wailord.org",
+    },
+  ],
+};
