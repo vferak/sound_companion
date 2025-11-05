cd8e2cb7b9502d6efc0f30b41ae221315f858fb7
Martin Borák <martin.borak24@gmail.com>
2025-11-05 12:24:39 +0100

    feat: more audio

--- src/boris-definition.ts
+++ src/boris-definition.ts
@@ -34,6 +34,44 @@ export const borisDefinitions = {
       name: "A abys věděl, že to myslím vážně",
       filename: "pobodam_fotra.mp3",
     },
+    {
+      name: "Hej hola, hospodo!",
+      filename: "hospoda.mp3",
+    },
+    {
+      name: "Berte si",
+      filename: "dlouhy.mp3",
+    },
+    {
+      name: "Děvenka moje starostlivá",
+      filename: "hospoda2.mp3",
+    },
+    /*
+    {
+      name: "Sound 4",
+      filename: "sound_4.mp3",
+    },
+    {
+      name: "Sound 5",
+      filename: "sound_5.mp3",
+    },
+    {
+      name: "Sound 6",
+      filename: "sound_6.mp3",
+    },
+    {
+      name: "Sound 7",
+      filename: "sound_7.mp3",
+    },
+    {
+      name: "Sound 8",
+      filename: "sound_8.mp3",
+    },
+    {
+      name: "Sound 9",
+      filename: "sound_9.mp3",
+    }*/
+    ,
   ],
   references: [
     {
